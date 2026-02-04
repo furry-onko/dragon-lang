4a97d7472847bafdf3570fd980dc14fe6fde0ca7
furry-onko <cute.neko.boy.uwu@gmail.com>
2026-02-04 02:37:02 +0100

    Enhanced tokenizer functionality; added string handling and symbol stack management; improved token generation for square brackets (i'll add more tmrw)

--- src/interpreter/tokenizer.rs
+++ src/interpreter/tokenizer.rs
@@ -3,12 +3,74 @@ use crate::file;
 use std::process;
 
 pub fn start(path: &str) {
-	let file_content: Vec<Vec<char>> = file::read_file_vec(path);
+	let file_content: Vec<Vec<char>> = file::read_file_vec(&format!("{path}/src/main.drg"));
+	tokenize(file_content);
 }
 
 fn tokenize(content: Vec<Vec<char>>) -> Program {
 	for line in content {
-		continue;
+		let mut tokens: Vec<Tokens> = Vec::new();
+		let mut word = String::new();
+		let mut number = String::new();
+		let mut string = String::new();
+		
+		let mut in_string: bool = false;
+		let mut symbol_stack: Vec<char> = Vec::new();
+
+		for ch in line {
+			if ch == '\'' {
+				if in_string {
+					tokens.push(Tokens::String(
+						std::mem::take(&mut string)
+					));
+					in_string = false;
+				}
+				else {
+					in_string = true;
+				}
+				continue;
+			}
+			if in_string {
+				string.push(ch);
+				continue;
+			}
+
+			if ch == '/' {
+				if let Some('/') = symbol_stack.last() {
+					symbol_stack.pop();
+				}
+				else {
+					symbol_stack.push(ch);
+				}
+				continue;
+			}
+
+			match ch {
+				';' => {continue;}
+				'[' => {
+					symbol_stack.push(ch);
+					tokens.push(Tokens::SqBracketOpen);
+					continue;
+				}
+				']' => {
+					if let Some('[') = symbol_stack.last() {
+						symbol_stack.pop();
+						tokens.push(Tokens::SqBracketClose);
+						continue;
+					}
+					else {
+						todo!();
+						/* REFACTORING NEEDED
+							- loops from for to while
+							- add an error reporter/handler (lines, error types etc)
+						*/
+					}
+				},
+				// '' => {}
+				_ => (),
+			}
+		}
+		println!("{:?}", tokens);
 	}
 
 	process::exit(0); // to make compiler stfu
@@ -22,13 +84,15 @@ struct Line {
 	content: Vec<Tokens>,
 }
 
+#[derive(Debug)]
 enum Tokens {
-	Number(i32),	// 0-9
+	Number(i64),	// 0-9
 	Word(String),	// A-Z
-	
+	String(String),	
+
 	Underscore,		// _
 	
-	Plus, Minus, Slash, Asterisk, Equals, // + - / * =
+	Plus, Minus, Asterisk, Equals, // + - * =
 
 	BracketOpen, BracketClose,		// ()
 	SqBracketOpen, SqBracketClose,	// []
@@ -36,10 +100,10 @@ enum Tokens {
 
 	Returns,	// >
 	Colon,		// :
-	Quote,		// '
 	Dot,		// .
 	Comma,		// ,
 	Hash,		// #
 	Dollar,		// $
 	Percent,	// %
+	Unknown,
 }
